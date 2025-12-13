5d45272dc2e3d74cfe1adf344cf5bfb8fdd6dd3d
Ilan Ehrlich <ehrlich.ilan@gmail.com>
2025-12-13 16:03:24 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -5,3 +5,11 @@ function showBlog(id) {
   document.getElementById(id).classList.add("active");
   event.target.classList.add("active");
 }
+
+function showSection(id) {
+  document.querySelectorAll("section").forEach(s => s.classList.remove("active"));
+  document.querySelectorAll(".main-tab").forEach(b => b.classList.remove("active"));
+
+  document.getElementById(id).classList.add("active");
+  event.target.classList.add("active"); // <-- this adds the bold/underline
+}
